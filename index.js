9767651cb13fdd8ffe93104588aa8f0d4a586675
Lior <levydlior@gmail.com>
2022-05-16 15:06:18 -0600

    commit tree e3294762fb2a075a24eb1c2d1ee1129547244076

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+    const newArray = [...array].slice(0,2)
+    
+    return newArray
+}
+
+function returnLastTwoDrivers(array) {
+    let newArray = [...array]
+    const arrayLengthMinusTwo = newArray.length-2
+    const arrayLength = newArray.length
+     return newArray.slice(arrayLengthMinusTwo, arrayLength)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x) {
+    function returnedNumber(x) {
+      return Math.abs(x * x)
+    }
+    return returnedNumber
+}
+
+const something = function (x) {
+    return createFareMultiplier(x) 
+}
+function fareDoubler(something){
+    return something * 2
+}
+
+const anotherVar = function (x) {
+    return createFareMultiplier(x)
+}
+function fareTripler(anotherVar){
+    return anotherVar * 3
+}
+   
+
+function selectDifferentDrivers(array, aFunc){
+    if (aFunc === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(array)
+    }else{
+        return returnLastTwoDrivers(array)
+    }
+}
